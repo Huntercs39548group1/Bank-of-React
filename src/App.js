7624e21ed69a869cbcd09cc61ea6e6439cdaa964
skissele <skissele@yahoo.com>
2022-06-30 17:23:56 -0400

    added routing to debit view

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
 import Credits from './components/Credits';
+import Debits from './components/Debits';
 
 class App extends Component {
   constructor() {  // Create and initialize state
@@ -19,7 +20,8 @@ class App extends Component {
         userName: 'Joe Smith',
         memberSince: '11/22/99',
       },
-      creditData: []
+      creditData: [],
+      debitData: []
     }
   }
 
@@ -33,18 +35,28 @@ class App extends Component {
   //Make API call to get credit data
   async  componentDidMount() {
     let linkToCreditData = 'https://moj-api.herokuapp.com/credits';
+    let linkToDebitData = 'https://moj-api.herokuapp.com/debits';
 
     try {
       let responseForCredit = await axios.get(linkToCreditData);
-      console.log(responseForCredit);
+      let responseForDebit = await axios.get(linkToDebitData);
+      console.log(responseForCredit);     
       for (let i = 0; i < responseForCredit.data.length; i++) {
         this.state.creditData.push(responseForCredit.data[i]);
+      }
+      console.log(responseForDebit);
+      for (let i = 0; i < responseForDebit.data.length; i++) {
+        this.state.debitData.push(responseForDebit.data[i]);
       }
     } catch (error) {
       if (error.responseForCredit) {
         console.log(error.responseForCredit.data);
         console.log(error.responseForCredit.status);
       }
+      if (error.responseForDebit) {
+        console.log(error.responseForDebit.data);
+        console.log(error.responseForDebit.status);
+      } 
     }
   }
 
@@ -57,7 +69,8 @@ class App extends Component {
     );
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);  // Pass props to "LogIn" component
     const CreditsComponent= () => (<Credits creditData={this.state.creditData} />);
-
+    const DebitsComponent = () => (<Debits debitData={this.state.debitData}/>);
+    
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
       <Router basename="/bank-of-react-example-code-gh-pages">
@@ -66,6 +79,7 @@ class App extends Component {
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/login" render={LogInComponent}/>
           <Route exact path="/credits" render={CreditsComponent}/>
+          <Route exact path="/debits" render={DebitsComponent}/>
         </div>
       </Router>
     );
