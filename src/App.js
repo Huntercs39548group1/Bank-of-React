7a7371e59aa1aee6e418417fd48c05f390fee065
remit233 <joeytan233@gmail.com>
2022-06-30 20:00:26 -0400

    Basename back to initial

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/progressgroup">
+      <Router basename="/bank-of-react-example-code-gh-pages">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
