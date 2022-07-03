60fad606796d76cf0c1a09bf7146ffe0e6ecf151
dive0 <zhengshiwei5@gmail.com>
2022-07-03 16:55:57 -0500

    Fixed bug in debits

--- src/App.js
+++ src/App.js
@@ -1,106 +1,140 @@
 // src/App.js
 
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
 
 // Import other components
-import Home from './components/Home';
-import UserProfile from './components/UserProfile';
-import LogIn from './components/Login';
-import Credits from './components/Credits';
-import Debits from './components/Debits';
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
+import LogIn from "./components/Login";
+import Credits from "./components/Credits";
+import Debits from "./components/Debits";
 
 class App extends Component {
-  constructor() {  // Create and initialize state
-    super(); 
-    this.state = {
-      accountBalance: 1234567.89,
-      currentUser: {
-        userName: 'Joe Smith',
-        memberSince: '11/22/99',
-      },
-      creditData: [],
-      debitData: []
+    constructor() {
+        // Create and initialize state
+        super();
+        this.state = {
+            accountBalance: 1234567.89,
+            currentUser: {
+                userName: "Joe Smith",
+                memberSince: "11/22/99",
+            },
+            creditData: [],
+            debitData: [],
+        };
     }
-  }
 
-  // Update state's currentUser (userName) after "Log In" button is clicked
-  mockLogIn = (logInInfo) => {  
-    const newUser = {...this.state.currentUser}
-    newUser.userName = logInInfo.userName
-    this.setState({currentUser: newUser})
-  }
+    // Update state's currentUser (userName) after "Log In" button is clicked
+    mockLogIn = (logInInfo) => {
+        const newUser = { ...this.state.currentUser };
+        newUser.userName = logInInfo.userName;
+        this.setState({ currentUser: newUser });
+    };
 
-  sendCredit = (newCredit)=> {
-  
-    this.setState({...this.state.creditData, newCredit})
+    sendCredit = (newCredit) => {
+        this.setState({ ...this.state.creditData, newCredit });
+    };
 
-  }
-
-  sendDebit = (newDebit)=> {
-  
-    this.setState({...this.state.debitData, newDebit})
-
-  }
-
-  //Make API call to get credit data
-  async  componentDidMount() {
-    let linkToCreditData = 'https://moj-api.herokuapp.com/credits';
-    let linkToDebitData = 'https://moj-api.herokuapp.com/debits';
-
-    try {
-      let responseForCredit = await axios.get(linkToCreditData);
-      let responseForDebit = await axios.get(linkToDebitData);
-      console.log(responseForCredit);
-      for (let i = 0; i < responseForCredit.data.length; i++) {
-        this.state.creditData.push(responseForCredit.data[i]);
+    sendDebit = (newDebit, apiVal) => {
+      if (newDebit === undefined) {
+        this.setState(() => {
+          return {
+            accountBalance: apiVal,
+        };
+        });        
       }
-      console.log(responseForDebit);
-      for (let i = 0; i < responseForDebit.data.length; i++) {
-        this.state.debitData.push(responseForDebit.data[i]);
-      }
-    } catch (error) {
-      if (error.responseForCredit) {
-        console.log(error.responseForCredit.data);
-        console.log(error.responseForCredit.status);
-      }
-      if (error.responseForDebit) {
-        console.log(error.responseForDebit.data);
-        console.log(error.responseForDebit.status);
-      } 
+        else if (newDebit && newDebit.length > 0) {
+            let item = newDebit[newDebit.length - 1];
+
+            this.setState((prevState) => {
+                // return {...prevState, accountBalance : Number(item.amount) + prevState.accountBalance}
+                return {
+                    prevState,
+                    accountBalance:
+                      prevState.accountBalance - Number(item.amount),
+                };
+            });
+        }
+    };
+
+    //Make API call to get credit data
+    async componentDidMount() {
+        let linkToCreditData = "https://moj-api.herokuapp.com/credits";
+        let linkToDebitData = "https://moj-api.herokuapp.com/debits";
+
+        try {
+            let responseForCredit = await axios.get(linkToCreditData);
+            let responseForDebit = await axios.get(linkToDebitData);
+            console.log(responseForCredit);
+            for (let i = 0; i < responseForCredit.data.length; i++) {
+                this.state.creditData.push(responseForCredit.data[i]);
+            }
+            console.log(responseForDebit);
+            for (let i = 0; i < responseForDebit.data.length; i++) {
+                this.state.debitData.push(responseForDebit.data[i]);
+            }
+        } catch (error) {
+            if (error.responseForCredit) {
+                console.log(error.responseForCredit.data);
+                console.log(error.responseForCredit.status);
+            }
+            if (error.responseForDebit) {
+                console.log(error.responseForDebit.data);
+                console.log(error.responseForDebit.status);
+            }
+        }
     }
-  }
 
-  // Create Routes and React elements to be rendered using React components
-  render() {  
-    // Create React elements
-    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
-    const UserProfileComponent = () => (
-      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
-    );
-    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);  // Pass props to "LogIn" component
-    
-    const CreditsComponent= () => (<Credits creditData={this.state.creditData}
-       accountBalance={this.state.accountBalance} 
-       setState={this.sendCredit}/>);
+    // Create Routes and React elements to be rendered using React components
+    render() {
+        // Create React elements
+        const HomeComponent = () => (
+            <Home accountBalance={this.state.accountBalance} />
+        );
+        const UserProfileComponent = () => (
+            <UserProfile
+                userName={this.state.currentUser.userName}
+                memberSince={this.state.currentUser.memberSince}
+            />
+        );
+        const LogInComponent = () => (
+            <LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />
+        ); // Pass props to "LogIn" component
 
-       const DebitsComponent= () => (<Debits debitData={this.state.debitData} 
-        accountBalance={this.state.accountBalance}
-        setState={this.sendDebit}/>);
-    // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
-    return (
-      <Router basename="/BANK-OF-REACT/">
-        <div>
-          <Route exact path="/" render={HomeComponent}/>
-          <Route exact path="/userProfile" render={UserProfileComponent}/>
-          <Route exact path="/login" render={LogInComponent}/>
-          <Route exact path="/credits" render={CreditsComponent}/>
-          <Route exact path="/debits" render={DebitsComponent}/>
-        </div>
-      </Router>
-    );
-  }
+        const CreditsComponent = () => (
+            <Credits
+                creditData={this.state.creditData}
+                accountBalance={this.state.accountBalance}
+                setState={this.sendCredit}
+            />
+        );
+
+        const DebitsComponent = () => (
+            <Debits
+                debitData={this.state.debitData}
+                accountBalance={this.state.accountBalance}
+                setState={this.sendDebit}
+            />
+        );
+        // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
+        return (
+            <Router basename="/BANK-OF-REACT/">
+                <div>
+                    <Route exact path="/" render={HomeComponent} />
+                    <Route
+                        exact
+                        path="/userProfile"
+                        render={UserProfileComponent}
+                    />
+                    <Route exact path="/login" render={LogInComponent} />
+                    <Route exact path="/credits" render={CreditsComponent} />
+                    <Route exact path="/debits" render={DebitsComponent} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,76 +1,104 @@
 // src/components/Debits.js
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import AccountBalance from './AccountBalance';
-import { v1 as uuidv1 } from 'uuid';
-import { isArray } from 'lodash';
-import moment from 'moment';
+import React, { useState, useEffect, useRef  } from "react";
+import { Link } from "react-router-dom";
+import AccountBalance from "./AccountBalance";
+import { v1 as uuidv1 } from "uuid";
+import { isArray } from "lodash";
+import moment from "moment";
 
-const Debits = ({debitData, accountBalance, setState}) => {
-  // Create the list of Debit items
-  const [totalDebit, setTotalDebit] = useState([...debitData]);
-  const [description, setDescription] = useState('');
-  const [debitAmount, setDebitAmount] = useState(0);
-  console.log("totalDebit", totalDebit)
-	let debitsView = () => {
-    if (isArray(totalDebit)) {
-      return totalDebit.map((debit) => { 
-        return <li key={debit.id}> {debit.amount} {debit.description} {moment(debit.date).format('YYYY-MM-DD')}</li>
-      })
-    } 
-  }
-  
-  //Once Add Debit clicked send description and debitAmount to 
-  //Array. 
-  let handleSubmit = (e) =>{
-    e.preventDefault();
-    const newObj = {
-      id: uuidv1(),
-      description: description,
-      amount: debitAmount,
-      date: moment(new Date()).format('YYYY-MM-DD'),
-    }
-    setTotalDebit([...totalDebit, newObj])
-    setState(totalDebit);
-  }
-  
-  // Render the list of Debit items and a form to input new Debit item
-  return (
-    <div>
-      <h1>Debits</h1>
-      {debitsView()}
+const Debits = ({ debitData, accountBalance, setState }) => {
+    // Create the list of Debit items
+    const [totalDebit, setTotalDebit] = useState([...debitData]);
+    const [description, setDescription] = useState("");
+    const [debitAmount, setDebitAmount] = useState(0);
+    // const [apibalance, setApiBalance] = useState(0);
+    
+    let debitsView = () => {
+        if (isArray(totalDebit)) {
+            return totalDebit.map((debit) => {
 
-      <form onSubmit={handleSubmit} >
-      <div>Description:
-      <input 
-      type="description" 
-      name="description" 
-      required = "required"
-      placeholder= "Enter a description"
-      value={description} 
-      onChange={(event) =>setDescription(event.target.value)} 
-      />
-      </div>
+                return (
+              // setApiBalance(apibalance + debit.amount),
 
-      <div>Debit:
-      <input 
-      type="debits" 
-      name="debits"
-      value={debitAmount}     
-      onChange={(e) => setDebitAmount(e.target.value)}
-      />
-      </div>
+                    <li key={debit.id}>
+                        {" "}
+                        {debit.amount} {debit.description}{" "}
+                        {moment(debit.date).format("YYYY-MM-DD")}
+                    </li>
+                );
+            });
+        }
+    };
+    // console.log(apibalance);
+    // setState(undefined, apibalance)
 
-      <button>Add debit</button>
-      </form>
+    //Once Add Debit clicked send description and debitAmount to
+    //Array.
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        const newObj = {
+            id: uuidv1(),
+            description: description,
+            amount: debitAmount,
+            date: moment(new Date()).format("YYYY-MM-DD"),
+        };
+        setTotalDebit([...totalDebit, newObj]);
+        // console.log({ debitAmount, description, totalDebit });
 
-    <div>      
-    <Link to="/">Return to Home</Link>
-    </div>
+        debitData.push(newObj);
+    };
 
-    <AccountBalance accountBalance={accountBalance} />
-    </div>
-  )
-}
+    const isFirstRender = useRef(true);
+
+    useEffect(() => {
+        // console.log({ totalDebit });
+        if (isFirstRender.current) {
+          isFirstRender.current = false;
+          return; // 👈️ return early if first render
+        }
+        setState(totalDebit);
+    }, [totalDebit, setState]);
+
+    // Render the list of Debit items and a form to input new Debit item
+    return (
+        <div>
+            <h1>Debits</h1>
+            {debitsView()}
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    Description:
+                    <input
+                        type="description"
+                        name="description"
+                        required="required"
+                        placeholder="Enter a description"
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                    />
+                </div>
+
+                <div>
+                    Debit:
+                    <input
+                        type="debits"
+                        name="debits"
+                        value={debitAmount}
+                        onChange={(e) => setDebitAmount(e.target.value)}
+                    />
+                </div>
+
+                <button>Add debit</button>
+                {/* onClick={() => setState(totalDebit)} */}
+            </form>
+
+            <div>
+                <Link to="/">Return to Home</Link>
+            </div>
+
+            <AccountBalance accountBalance={accountBalance} />
+        </div>
+    );
+};
 
 export default Debits;
